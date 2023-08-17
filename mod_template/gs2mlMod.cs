c685b9b7fafb8a645c8b03fc0db0d706cafea12a
Mattie Nash <mnashkittycat@gmail.com>
2023-08-17 17:16:08 -0500

    Add Mod Template!
    This is the first big step imo to making gs2ml more usable for developers.
    Instructions for use will be put in the readme sometime.

---
+++ mod_template/gs2mlMod.cs
@@ -0,0 +1,14 @@
+﻿using UndertaleModLib;
+
+namespace GS2ML_MOD_NAME;
+
+public class GS2ML_MOD_NAME
+{
+    public void Load(int audiogroup, UndertaleData data)
+    {
+        if(audiogroup != 0){
+            return;
+        }
+        // Here, you can modify the data however you want. Go nuts!
+    }
+}
