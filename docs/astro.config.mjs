3c43e1cecaeb5b6782aea6f06fd65cec9559e7b3
Mattie Nash <mnashkittycat@gmail.com>
2023-09-07 14:13:03 +0000

    Fix link in sidebar

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig({
 					label: 'Guides',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', link: '/guides/example/' },
+						{ label: 'Install Guide', link: '/guides/installation/' },
 					],
 				},
 				{
